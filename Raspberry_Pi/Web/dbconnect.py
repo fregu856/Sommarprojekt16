b4ef13ac004300a0f41a8c22a875528a9ea3b595
Fredrik Gustafsson <fregu856@student.liu.se>
2016-08-14 18:07:17 +0200

    Delete dbconnect.py

--- Raspberry_Pi/Web/dbconnect.py
+++
@@ -1,7 +0,0 @@
-import MySQLdb
-
-def connect():
-    connection = MySQLdb.connect(host="localhost", user="pi", passwd="raspberry", db="SQL_DATA")
-    cursor = connection.cursor()
-    
-    return cursor, connection
